8f3a92d94053114ab9ef756c92a9df5392742f99
Pablo The Penguin <pablo12335@gmail.com>
2023-09-01 19:16:37 -0700

    move to right area

--- scripts/deployVotingEscrow.ts
+++ scripts/deployVotingEscrow.ts
@@ -2,7 +2,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const tokenAddress = "0xYourTokenAddressHere"; // Replace with your token's address
+  const tokenAddress = "0x39eB558131E5eBeb9f76a6cbf6898f6E6DCe5e4E"; // QiDAO Governance Pool
   const name = "Advanced Voting Escrowed QI";
   const symbol = "aveQI";
   const authorizerAdaptor = "0xYourAuthorizerAdaptorAddressHere"; // Replace with your AuthorizerAdaptor's address
