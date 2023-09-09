d9e824a870db730b302c736758b65c6de100f036
Pablo <pablo@hashingsystems.com>
2023-09-09 01:52:43 -0500

    updated for solidity

--- scripts/deployVotingEscrow.ts
+++ scripts/deployVotingEscrow.ts
@@ -1,17 +1,21 @@
-module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+const hre = require("hardhat");
 
-  const tokenAddress = "0x39eB558131E5eBeb9f76a6cbf6898f6E6DCe5e4E"; // QiDAO Governance Pool
-  const name = "Advanced Voting Escrowed QI";
-  const symbol = "aveQI";
-  const authorizerAdaptor = "0xYourAuthorizerAdaptorAddressHere"; // Replace with your AuthorizerAdaptor's address
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
 
-  await deploy("VotingEscrow", {
-    from: deployer,
-    args: [tokenAddress, name, symbol, authorizerAdaptor],
-    log: true,
-  });
-};
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-module.exports.tags = ["VotingEscrow"];
+    const FeeDistributor = await hre.ethers.getContractFactory("VotingEscrow");
+    const feeDistributor = await FeeDistributor.deploy();
+
+    await feeDistributor.deployed();
+
+    console.log("Voting Escrow deployed to:", feeDistributor.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
