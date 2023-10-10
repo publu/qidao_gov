38e1ea3af4666b9265b30044db7dc689beac03ad
Pablo The Penguin <pablo12335@gmail.com>
2023-10-10 19:57:08 +0700

    deploy and initialize in one

--- scripts/deployRewardDist.ts
+++ scripts/deployRewardDist.ts
@@ -5,13 +5,20 @@ async function main() {
 
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
-    
+
     const RewardDistributor = await hre.ethers.getContractFactory("RewardDistributor");
     const rewardDistributor = await RewardDistributor.deploy();
 
     await rewardDistributor.deployed();
 
     console.log("RewardDistributor deployed to:", rewardDistributor.address);
+
+    const votingEscrowAddress = "0x12345";
+    const startTime = "1000000000"
+    const admin = "0x67890";
+
+    await rewardDistributor.initialize(votingEscrowAddress, startTime, admin);
+    console.log("RewardDistributor initialized");
 }
 
 main()
